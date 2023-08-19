8b518bfd37707a3eb1f6cf1eafe48e71fffac75f
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-19 08:10:35 +0200

    done first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-mnnmnmnn;
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', genColor);
+
+function genColor(evt) {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+}
+
+stopBtn.addEventListener('click', stoGenCol);
+
+function stoGenCol(evt) {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+}
