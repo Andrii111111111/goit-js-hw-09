f368f6b12776fc88bf41fbe851e14c4f3eb5e8cb
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-27 20:46:22 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,45 +5,34 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', generate);
 function generate(evt) {
   evt.preventDefault();
-  const amount = form.elements[2].value;
-  const step = form.elements[1].value;
-  const delay = form.elements[0].value;
+  const amount = evt.target.elements.amount.value;
+  const step = Number(evt.target.elements.step.value);
+  let delay = Number(evt.target.elements.delay.value);
 
   for (let i = 1; i <= amount; i += 1) {
-    if (i === 1) {
-      createPromise(i, delay)
-        .then(({ position, delay }) => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-    }
-    if (i > 1) {
-      setTimeout(() => {
-        createPromise(i, Number(delay) + Number(step) * i)
-          .then(({ position, delay }) => {
-            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-          })
-          .catch(({ position, delay }) => {
-            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-          });
-      }, step * i);
-    }
+    console.log(i);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
 
-  function createPromise(position, delay) {
-    return new Promise((fulfill, reject) => {
-      const shouldResolve = Math.random() > 0.3;
-      setTimeout(() => {
-        if (shouldResolve) {
-          // Fulfill
-          fulfill({ position, delay });
-        } else {
-          // Reject
-          reject({ position, delay });
-        }
-      }, delay);
-    });
-  }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
